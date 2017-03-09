d3c5a83e401aadb9060dc794ad24f2cade443396
Michael <weirdnamemike@gmail.com>
2017-03-08 20:36:28 -0800

    first coffeerun push

---
+++ coffeerun/scripts/tests.js
@@ -0,0 +1,63 @@
+QUnit.test("hello test", function(assert) {
+    assert.ok(1 == "1", "Passed!");
+});
+
+QUnit.test("Testing DataStore Methods", function(assert) {
+    var ds = new App.DataStore();
+
+    ds.add('m@bond.com', 'tea');
+    ds.add('james@bond.com', 'eshpressho');
+    assert.deepEqual(ds.getAll(), {
+        "james@bond.com": "eshpressho",
+        "m@bond.com": "tea"
+    }, "got everything")
+
+    ds.remove('james@bond.com');
+    assert.deepEqual(ds.getAll(), {
+        "m@bond.com": "tea"
+    }, "removed james@bond")
+
+    assert.equal(ds.get('m@bond.com'), "tea", "got the order for m@bond");
+
+    assert.equal(ds.get('james@bond.com'), undefined, "james@bond order is gone");
+});
+
+QUnit.test("Testing Truck Methods", function(assert) {
+
+
+    myTruck.createOrder({
+        emailAddress: 'me@goldfinger.com',
+        coffee: 'double mocha'
+    });
+    myTruck.createOrder({
+        emailAddress: 'dr@no.com',
+        coffee: 'decaf'
+    });
+    myTruck.createOrder({
+        emailAddress: 'm@bond.com',
+        coffee: 'earl grey'
+    });
+
+    assert.deepEqual(myTruck.printOrders(), [{
+                "coffee": "double mocha",
+                "emailAddress": "me@goldfinger.com"
+            },
+            {
+                "coffee": "decaf",
+                "emailAddress": "dr@no.com"
+            },
+            {
+                "coffee": "earl grey",
+                "emailAddress": "m@bond.com"
+            }
+        ],
+        "created all the 3 orders")
+
+    myTruck.deliverOrder('dr@no.com');
+    myTruck.deliverOrder('m@bond.com');
+
+    assert.deepEqual(myTruck.printOrders(), [{
+        "coffee": "double mocha",
+        "emailAddress": "me@goldfinger.com"
+    }], "delievered 2 orders");
+});
