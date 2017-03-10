2dcccc5d18f76383d9657a70e97dbcbb2ea1855a
Michael <weirdnamemike@gmail.com>
2017-03-09 18:31:18 -0800

    eslint

--- coffeerun/scripts/tests.js
+++ coffeerun/scripts/tests.js
@@ -1,29 +1,31 @@
-QUnit.test("hello test", function(assert) {
-    assert.ok(1 == "1", "Passed!");
+QUnit.test('hello test', function(assert) {
+    assert.ok(1 == '1', 'Passed!');
 });
 
-QUnit.test("Testing DataStore Methods", function(assert) {
+QUnit.test('Testing DataStore Methods', function(assert) {
+    var App = window.App;
     var ds = new App.DataStore();
 
     ds.add('m@bond.com', 'tea');
     ds.add('james@bond.com', 'eshpressho');
     assert.deepEqual(ds.getAll(), {
-        "james@bond.com": "eshpressho",
-        "m@bond.com": "tea"
-    }, "got everything")
+        'james@bond.com': 'eshpressho',
+        'm@bond.com': 'tea'
+    }, 'got everything');
 
     ds.remove('james@bond.com');
     assert.deepEqual(ds.getAll(), {
-        "m@bond.com": "tea"
-    }, "removed james@bond")
+        'm@bond.com': 'tea'
+    }, 'removed james@bond');
 
-    assert.equal(ds.get('m@bond.com'), "tea", "got the order for m@bond");
+    assert.equal(ds.get('m@bond.com'), 'tea', 'got the order for m@bond');
 
-    assert.equal(ds.get('james@bond.com'), undefined, "james@bond order is gone");
+    assert.equal(ds.get('james@bond.com'), undefined, 'james@bond order is gone');
 });
 
-QUnit.test("Testing Truck Methods", function(assert) {
-
+QUnit.test('Testing Truck Methods', function(assert) {
+    var App = window.App;
+    var myTruck = App.Truck;
 
     myTruck.createOrder({
         emailAddress: 'me@goldfinger.com',
@@ -39,27 +41,27 @@ QUnit.test("Testing Truck Methods", function(assert) {
     });
 
     assert.deepEqual(myTruck.printOrders(), [{
-                "coffee": "double mocha",
-                "emailAddress": "me@goldfinger.com"
-            },
-            {
-                "coffee": "decaf",
-                "emailAddress": "dr@no.com"
-            },
-            {
-                "coffee": "earl grey",
-                "emailAddress": "m@bond.com"
-            }
-        ],
-        "created all the 3 orders")
+        'coffee': 'double mocha',
+        'emailAddress': 'me@goldfinger.com'
+    },
+    {
+        'coffee': 'decaf',
+        'emailAddress': 'dr@no.com'
+    },
+    {
+        'coffee': 'earl grey',
+        'emailAddress': 'm@bond.com'
+    }
+    ],
+        'created all the 3 orders');
 
     myTruck.deliverOrder('dr@no.com');
     myTruck.deliverOrder('m@bond.com');
 
     assert.deepEqual(myTruck.printOrders(), [{
-        "coffee": "double mocha",
-        "emailAddress": "me@goldfinger.com"
-    }], "delievered 2 orders");
+        'coffee': 'double mocha',
+        'emailAddress': 'me@goldfinger.com'
+    }], 'delievered 2 orders');
 });
 
 //The problem with the truck module was that printOrders didn't return anything, so we had to go in and change that
